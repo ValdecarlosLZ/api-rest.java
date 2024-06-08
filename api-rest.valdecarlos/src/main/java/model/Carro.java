d450d21d9484612fad3bac6eda068a4b8530b407
Valdecarlos Henrique Garcia Dos Santos - FATEC - Cuiaba/MT <valdecarlos.santos@alunosenai.mt>
2024-06-07 21:03:25 -0400

    Java API Rest

---
+++ api-rest.valdecarlos/src/main/java/model/Carro.java
@@ -0,0 +1,40 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+    private String nome;
+    private String marca;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+}
---
+++ api-rest.valdecarlos/src/test/java/br/luxzero/api_rest/valdecarlos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.luxzero.api_rest.valdecarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
